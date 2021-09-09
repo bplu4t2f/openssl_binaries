2e5801505c022428029d628ef45c01462541ea96
bplu4t2f <bplu4t2f@gmail.com>
2021-09-10 00:02:12 +0200

    Added the things.

---
+++ make_libs.cmd
@@ -0,0 +1,5 @@
+lib /def:x86\libssl.def /machine:x86 /out:x86\libssl.lib
+lib /def:x86\libcrypto.def /machine:x86 /out:x86\libcrypto.lib
+
+lib /def:x64\libssl.def /machine:x64 /out:x64\libssl.lib
+lib /def:x64\libcrypto.def /machine:x64 /out:x64\libcrypto.lib
